a3a84a5ea78e715c08133e678e3c77167eea531f
WielkiMarsz <bok@listy-slubne.pl>
2014-07-18 17:44:43 +0200

    rurkowce

--- www/js/index.js
+++ www/js/index.js
@@ -88,7 +88,15 @@ function onPhotoDataSuccess(imageData) {
 }
 function playAudio(url) {
     // Play the audio file at url
-    var my_media = new Media(url});
+    var my_media = new Media(url,
+        // success callback
+        function() {
+            
+        },
+        // error callback
+        function(err) {
+        	
+    });
 
     // Play audio
     my_media.play();
